3a0d10aa3f46ac461fcedf3f34588ba1d0c24459
Ben Smith <benjaminsmith@ksu.edu>
2025-11-18 14:07:05 -0600

    Added trie update function and window update as well

--- Trie.cpp
+++ Trie.cpp
@@ -1,1 +1,55 @@
+#include <cstdint>
+
 #define length 26
+
+
+class Trie 
+{
+    public:
+    uint64_t gram_1[length] = {0};
+    uint64_t gram_2[length * length] = {0};
+    uint64_t gram_3[length * length * length] = {0};
+    uint64_t gram_4[length * length * length * length] = {0};
+
+    /// @brief This will update the trie's 1,2,3,4 grams
+    /// @param s is a string with 4 lowercase letters only
+    /// @return this will return false if there are not enough letters, else true;
+    bool AddWindowGrams(const std:string& s)
+    {
+        //make sure string has only 4 chars
+        if(s.size() != 4)
+        {
+            return false;
+        }
+
+        //find the index of the first gram
+        uint32_t index = val(s[0]);
+        //increment the 1 gram
+        gram_1[index]++;
+
+        //find the index of the second gram
+        index = 26*index + val(s[1]);
+        //increment the 2 gram
+        gram_2[index]++;
+
+        //find the index of the third gram
+        index = 26*index + val(s[2]);
+        //increment the 3 gram
+        gram_3[index]++;
+
+        //find the index of the fourth gram
+        index = 26*index + val(s[3]);
+        //increment the 4 gram
+        gram_4[index]++;
+
+        return true;
+    }
+};
+
+/// @brief This turns the input char to a uint32_t where 'a' = 0
+/// @param c the char value to be calculated
+/// @return the uint32_t value of the input char
+inline uint32_t val(char c)
+{
+    return uint32_t(c - 'a');
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <cstdio>
+#include "Trie.cpp"
+
 int main()
 {
     printf("hello world");
+
+    //this is the sliding window
+    std::string window;
+    window.reserve(4);
+}
+
+/// @brief This updates the window for the trie
+/// @param s the address of the string bufffer
+/// @param nextLeter the letter to be added
+void updateWindow(std::string& s, char nextLeter)
+{
+    s[0] = s[1];
+    s[1] = s[2];
+    s[2] = s[3];
+    s[3] = nextLeter;
 }
